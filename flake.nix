5ba92cd8216e2e6a135d77df516a87c5715e2a5e
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-05 17:37:28 +0400

    chore: project scaffolding
    Add LICENSE, version, nix dev shell, gitignore, and GitHub Actions
    workflows for CI testing and release-please release automation.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "gh-worktree development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bash
+            gh
+            gum
+            jq
+            bats
+            shellcheck
+          ];
+        };
+      }
+    );
+}
