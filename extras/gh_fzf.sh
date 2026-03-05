77e0047514152b07b12fcc6888f6aeb387b64afc
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-05 21:35:08 +0400

    fix: use execute-silent + detach + switch-client to avoid TTY error

--- extras/gh_fzf.sh
+++ extras/gh_fzf.sh
@@ -20,19 +20,19 @@ if [[ -z "${TMUX:-}" ]]; then
 fi
 
 _gh_fzf_pr_opts=(
-	'--bind "alt-S:execute(tmux new-session -s pull-{1} gh worktree pr {1})"'
+	'--bind "alt-S:execute-silent(tmux new-session -d -s pull-{1} \"gh worktree pr {1}\" && tmux switch-client -t pull-{1})"'
 )
 export GH_FZF_PR_OPTS="${GH_FZF_PR_OPTS:+${GH_FZF_PR_OPTS} }${_gh_fzf_pr_opts[*]}"
 unset _gh_fzf_pr_opts
 
 _gh_fzf_issue_opts=(
-	'--bind "alt-S:execute(tmux new-session -s issue-{1} gh worktree issue {1})"'
+	'--bind "alt-S:execute-silent(tmux new-session -d -s issue-{1} \"gh worktree issue {1}\" && tmux switch-client -t issue-{1})"'
 )
 export GH_FZF_ISSUE_OPTS="${GH_FZF_ISSUE_OPTS:+${GH_FZF_ISSUE_OPTS} }${_gh_fzf_issue_opts[*]}"
 unset _gh_fzf_issue_opts
 
 _gh_fzf_run_opts=(
-	'--bind "alt-S:execute(tmux new-session -s run-{-1} gh worktree run {-1})"'
+	'--bind "alt-S:execute-silent(tmux new-session -d -s run-{-1} \"gh worktree run {-1}\" && tmux switch-client -t run-{-1})"'
 )
 export GH_FZF_RUN_OPTS="${GH_FZF_RUN_OPTS:+${GH_FZF_RUN_OPTS} }${_gh_fzf_run_opts[*]}"
 unset _gh_fzf_run_opts
